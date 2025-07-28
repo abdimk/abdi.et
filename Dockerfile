75a15670e9563c9c83d5bb2a15838a0c68acb24e
abdimk <abdisamk@gmail.com>
2025-07-28 21:40:08 +0300

    updated resume link

---
+++ Dockerfile
@@ -0,0 +1,24 @@
+FROM node:20-alpine AS build
+
+WORKDIR /app
+
+COPY package*.json ./
+
+RUN npm install --frozen-lockfile
+
+COPY . .
+
+RUN npm run build
+
+FROM nginx:alpine
+
+COPY --from=build /app/dist /usr/share/nginx/html
+
+RUN rm /etc/nginx/conf.d/default.conf
+
+COPY nginx.conf /etc/nginx/conf.d/
+
+EXPOSE 80
+
+
+CMD ["nginx", "-g", "daemon off;"]
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -6,25 +6,25 @@ const experienceData = [
     jobTitle: "Backend / DevOps at Nilode",
     location: "(remote)",
     date: "Mar 2025 - Present",
-    description: "Currently leading backend development and DevOps initiatives, focusing on scalable microservices and robust deployment pipelines using Kubernetes and CI/CD best practices. Working with Python and Go."
+    description: "Responsible for developing various backend systems with a strong focus on web scraping and automation using Python.Worked extensively with technologies such as Next.js,Puppeteer,Selenium, FastAPI, PostgreSQL, and Docker."
   },
   {
     jobTitle: "Machine Learning Engineer",
     location: "at INSA (Intership onsite)",
     date: "July 2024 - Sep 2024",
-    description: "Developed and deployed machine learning models for image recognition and natural language processing. Focused on data preprocessing, model training, and integration into existing systems. Utilized TensorFlow and PyTorch."
+    description: "Developed a distributed facial recognition and activity tracking attendance system.The system leverages Apache Kafka to stream video from multiple CCTV cameras, processes the data through a TensorFlow model, retrieves user information from SQLite"
   },
   {
     jobTitle: "Automation Engineer and Web Dev",
     location: "at Ethio aliance (onsite / remote)",
     date: "Oct 2023 - May 2024",
-    description: "Designed and implemented automation scripts to streamline internal processes, significantly reducing manual effort. Also contributed to front-end and backend web development using Django and React, improving user interfaces and API performance."
+    description: "Initiated continuous improvement and efficiency gains by automating the filtering and processing of 75,000 business records, eliminating hours of manual work. The Business Directory API, built using FastAPI, Uvicorn, and JWT, resulted in improved data accuracy and company performance."
   },
   {
     jobTitle: "BackEnd Engineer",
     location: "at Scale AI (remote)",
     date: "Mar 2022 - Sep 2022",
-    description: "Contributed to building high-performance backend services for data annotation pipelines. Focused on optimizing database queries and API endpoints to handle large volumes of data efficiently. Gained experience in distributed systems and cloud environments."
+    description: "Contributed to building high-performance backend services for data annotation pipelines, focusing on optimizing database queries and API endpoints to efficiently handle large-scale data. Worked as a Backend Engineer using FastAPI, Django, and Flask, and developed NLP applications with Python and Julia. Gained experience in distributed systems and cloud environments."
   },
 ];
 
