9e6fdb62c201e2de69d603e10dae88b763f2b683
abdimk <abdisamerga@yahoo.com>
2026-06-21 13:08:18 +0300

    chore:deleteing stray files

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import LanguagesTools from "./components/LanguagesTools";
 import Projects from "./components/Projects";
 import Experience from "./components/Experience";
 import Footer from "./components/Footer";
+import { Analytics } from "@vercel/analytics/next"
 
 const App = () => {
   return (
