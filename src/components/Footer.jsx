b9383ce2a36bdbad313b487d45baa0358702820e
abdimk <abdisamerga@yahoo.com>
2026-07-12 14:06:15 +0300

    chore(footer):update the time

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -35,7 +35,7 @@ const Footer = () => {
 
   return (
     <footer className="mt-8 pt-8 border-t border-border-color text-sm text-center text-text-color flex flex-col sm:flex-row justify-between items-center">
-      <p className="text-base mb-2 sm:mb-0 ml-0 sm:ml-8">Made with ❤️ 2025 by Abdisa</p>
+      <p className="text-base mb-2 sm:mb-0 ml-0 sm:ml-8">Made with ❤️ 2026 by Abdisa</p>
       <p className="border border-gray-400 py-1 px-2 -mt-1 bg-gray-300 mr-0 sm:mr-8">
         Updated: {updateDate}
       </p>
